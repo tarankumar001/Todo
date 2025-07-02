a129b540b25074464f44ad1163d337753b2f7bf3
P.Tarankumar <140129581+tarankumar001@users.noreply.github.com>
2025-07-02 21:25:23 +0530

    Connected MongoDB Atlas and fixed backend connection

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,29 @@
-import express from 'express'
-import { connectDB } from './config/db.';
-import dotenv from 'dotenv';
-import todoRoutes from './route/todo.route.js';
-
+import express from "express";
+import dotenv from "dotenv";
 dotenv.config();
+import todoRoutes from "./routes/todo.route.js";
+import { connectDB } from './config/db.js';
+import cors from "cors";
+import path from "path";
+const PORT = process.env.PORT || 5000;
 
 const app = express();
-app.get('/', (req, res) => {
-    res.send('Hello, World!')
-});
 
 app.use(express.json());
-app.use('/api/todos',todoRoutes);
+// app.use(cors());
 
+app.use("/api/todos", todoRoutes);
 
-app.listen(5000, () => {
-    connectDB();
-    console.log('Server is running on port 5000')
+const __dirname = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
+
+app.listen(PORT, () => {
+  connectDB();
+  console.log("Server started at http://localhost:5000");
 });
-
-export default app
